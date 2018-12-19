5d7cdd288e73c251fca93409f54ebd60d28cf31c
Hagita Atsushi <hagita_a@HagitanoMacBook-ea.local>
2018-12-19 09:11:25 +0900

    add real estate calc

---
+++ realestate/main.js
@@ -0,0 +1,7 @@
+function calc() {
+    // ここに#buttonをクリックしたら発生させる処理を記述する
+    console.log("hello world")
+    let income = document.getElementById("input").getElementsByClassName("income").item(0)
+    console.log(income)
+  
+};
